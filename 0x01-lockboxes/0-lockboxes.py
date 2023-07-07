2bdde5519e642506f3d508c843f9e8c89daa5d56
janeamy <jane.ammie@gmail.com>
2023-07-07 14:07:10 -0700

    Task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Determine if all the boxes can be opened.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    n = len(boxes)
+    visited = set()
+    stack = [0]  # Start with the first box (box 0)
+
+    while stack:
+        box = stack.pop()
+        visited.add(box)
+
+        for key in boxes[box]:
+            if key < n and key not in visited:
+                stack.append(key)
+
+    return len(visited) == n
