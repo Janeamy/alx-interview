3a7245a44ffaf30e1cd9eea987471f616d4efa6f
Janeamy <111252343+Janeamy@users.noreply.github.com>
2023-09-24 22:39:44 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,37 @@
 #!/usr/bin/python3
-"""
-Log Parsing : script that reads stdin line by line and computes metrics:
-"""
+'''This is a script that reads stdin line by line and computes metrics.'''
+
+
 import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-total_file_size = 0
-status = ['200', '301', '400', '401', '403', '404', '405', '500']
-obj = dict.fromkeys(status, 0)
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-def printLogStat():
-    """
-    log stats
-    """
-    print("File size: {}".format(total_file_size))
-    for key, value in sorted(obj.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+except Exception as err:
+    pass
 
-
-if __name__ == "__main__":
-    count_stat = 0
-    try:
-        for line in sys.stdin:
-            line = line.split()
-            count_stat += 1
-            try:
-                total_file_size += int(line[-1])
-
-                if line[-2] in status:
-                    obj[line[-2]] += 1
-
-            except (IndexError, ValueError):
-                pass
-
-            if count_stat % 10 == 0:
-                printLogStat()
-    except KeyboardInterrupt:
-        printLogStat()
-        raise
-    else:
-        printLogStat()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
